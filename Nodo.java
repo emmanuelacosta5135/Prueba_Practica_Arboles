b3c8d03802d99c593031a6ed5e085023a0cc3a0a
emmanuelacosta5135 <eacosta5806@uta.edu.ec>
2026-05-15 08:18:23 -0500

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,15 +1,31 @@
 // ============================================================
-// Archivo: Nodo.java
+//  CLASE NODO
+//  Representa cada nodo del árbol binario
 // ============================================================
 
 public class Nodo {
 
+    // ---------------- ATRIBUTOS ----------------
+
+    // Datos almacenados dentro del nodo
     Estudiante datos;
+
+    // Referencia al hijo izquierdo
     Nodo izq;
+
+    // Referencia al hijo derecho
     Nodo der;
 
+
+    // ---------------- CONSTRUCTOR ----------------
+    // Crea un nodo con los datos del estudiante
+
     public Nodo(Estudiante datos) {
+
+        // Guarda el estudiante
         this.datos = datos;
+
+        // Inicialmente no tiene hijos
         this.izq = null;
         this.der = null;
     }
